b1a78a2b00940ead75287fb1b0b764117abf5b70
Михайло Горбань <mikegorban@MacBook-Air-Mihajlo.local>
2025-11-07 21:16:45 +0200

    refactor post_detail view and configure global templatesdirectory

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,5 +1,5 @@
 from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 def post_list(request):
@@ -7,10 +7,7 @@ def post_list(request):
     return render(request, 'blog/post/list.html', {'posts': posts})
 
 def post_detail(request, id):
-    try:
-        post = Post.published.get(id=id)
-    except Post.DoesNotExist:
-        raise Http404("Post not found.")
-    
+    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
     return render(request, 'blog/post/detail.html', {'post': post})
+
 

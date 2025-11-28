25727ec9d65f4decc58c565cfe047cdf2cf5f5c6
Михайло Горбань <mikegorban@MacBook-Air-Mihajlo.local>
2025-11-28 10:02:51 +0200

    convert post list to class-based view

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -2,21 +2,15 @@ from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 
 
-def post_list(request):
-   post_list = Post.published.all()
-   # Пагінація з 3 постами на сторінку
-   paginator = Paginator(post_list, 3)
-   page_number = request.GET.get('page', 1)
-   try:
-        posts = paginator.page(page_number)
-   except PageNotAnInteger:
-        posts = paginator.page(1)
-   except EmptyPage:
-        posts = paginator.page(paginator.num_pages)
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
-   return render(request, 'blog/post/list.html', {'posts': posts})
 
 
 

90de00dc0ffc07060024affb6e7b4fd6fca6b7da
Михайло Горбань <mikegorban@MacBook-Air-Mihajlo.local>
2025-11-28 09:34:23 +0200

    update post detail url pattern with date and slug

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -26,6 +26,14 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('blog:post_detail', args=[self.id])
+    
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug])
+
 
 
 
--- myblog/myblog/urls.py
+++ myblog/myblog/urls.py
@@ -17,7 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include  # include потрібен!
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),  # Підключаємо urls додатку blog
+   
+
 ]
+

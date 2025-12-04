7e37bcf6859c7ba9fb4ed112aeef806933333556
Михайло Горбань <mikegorban@MacBook-Air-Mihajlo.local>
2025-12-04 18:18:09 +0200

    implement post comment view

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,10 +1,13 @@
 from django.http import Http404
-from django.shortcuts import render, get_object_or_404
-from .models import Post
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from .forms import EmailPostForm
 from django.core.mail import send_mail
+from .forms import CommentForm
+from django.views.decorators.http import require_POST
+
 
 
 class PostListView(ListView):
@@ -43,5 +46,18 @@ def post_share(request, post_id):
     else:
         form = EmailPostForm()
     return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
+
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+    return render(request, 'blog/post/comment.html', {'post': post, 'form': form, 'comment': comment})
+
 
 
